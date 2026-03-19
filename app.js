d729ebaeeef2732ade8a839026d623dc6891f5fc
Harshinii <harshiniishankar18@gmail.com>
2026-03-19 22:32:35 +0530

    Mongodb connection setup

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+
+
+//db
+const connectDB = require('./db/connect')
+
+//route
+const authRouter = require('./routes/auth')
 
 //to get req.body
 app.use(express.json())
 
-app.get('/api/v1', (req, res)=>{
-  res.status(200).send("Hello there")
-})
+//middleware
+app.use('/api/v1/auth', authRouter)
 
 //port 
 const port = process.env.PORT || 4000
 
+//start fn
 const start = async()=>{
   try {
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, ()=>{
       console.log(`Server is listening on port ${port}...`)
     })
---
+++ controllers/auth.js
@@ -0,0 +1,13 @@
+
+const register = async(req, res)=>{
+  res.send('registered')
+}
+
+const login = async(req, res)=>{
+  res.send('logged in')
+}
+
+module.exports = {
+  register,
+  login
+}
