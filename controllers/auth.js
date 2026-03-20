0b1cbd938491b2a592e3c17a57cdb7f2a2266340
Harshinii <harshiniishankar18@gmail.com>
2026-03-20 19:28:17 +0530

    User validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const User = require('../models/User')
 
 const register = async(req, res)=>{
   res.send('registered')
@@ -7,6 +8,11 @@ const login = async(req, res)=>{
   res.send('logged in')
 }
 
+//create user demo
+// const createUser = async(req, res)=>{
+//   const user = await User.create(req.body)
+//   res.status(200).json(user)
+// }
 module.exports = {
   register,
   login
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const {register,
-  login} = require('../controllers/auth')
+  login,
+createUser} = require('../controllers/auth')
 
 router.post('/register',register)
 router.post('/login',login)
+
+//createUserDemo
+// router.post('/createuser',createUser)
 
 module.exports = router
