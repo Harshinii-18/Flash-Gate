9e4bc78736b11fa7b1821bc64203e7123fb258d9
Harshinii <harshiniishankar18@gmail.com>
2026-03-21 17:24:17 +0530

    fix(error): improve error handling middleware response

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,10 @@
 const {StatusCodes} = require('http-status-codes')
+const {CustomAPIError} = require('../errors')
 
 const errorHandlerMiddleware = (err, req, res, next)=>{
+  if(err instanceof CustomAPIError){
+    res.status(err.statusCode).json({msg : err.message})
+  }
   res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({err})
 }
 
