b40eadb82cb7df62f7e492a8a905daa30d5fd7df
Harshinii <harshiniishankar18@gmail.com>
2026-03-21 16:42:18 +0530

    feat(auth): Add register controller with jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,18 @@
 const User = require('../models/User')
+const jwt = require('jsonwebtoken')
+const {StatusCodes} =  require('http-status-codes')
 
 const register = async(req, res)=>{
-  res.send('registered')
+  const user = await User.create({...req.body})
+  const token = user.createJWT()
+  res.status(StatusCodes.CREATED).json({user :{id: user._id, name: user.name, email: user.email, role: user.role }, token})
+  
 }
 
 const login = async(req, res)=>{
   res.send('logged in')
 }
 
-//create user demo
-// const createUser = async(req, res)=>{
-//   const user = await User.create(req.body)
-//   res.status(200).json(user)
-// }
 module.exports = {
   register,
   login
